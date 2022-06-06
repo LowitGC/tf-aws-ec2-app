2487a92eedf61b7bc99598d38ce9550f616506cb
LowitGC <lohithgc@hotmail.com>
2022-06-06 23:56:16 +0100

    updated instance name for testing

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@ variable "aws_region" {
 variable "instance_name" {
   description = "Value of the Name tag for the EC2 instance"
   type        = string
-  default     = "ExampleAppServerInstance"
+  default     = "AppServerInstance-2"
 }
 
 variable "instance_type" {
